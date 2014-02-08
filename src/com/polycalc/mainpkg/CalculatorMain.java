4f753354df390a3ced55408cf25072c2712e18b6
jyoti ranjan pattnaik <jyotiranjanpattnaik@gmail.com>
2014-02-09 00:46:43 +0530

    Project Set up

---
+++ src/com/polycalc/mainpkg/CalculatorMain.java
@@ -0,0 +1,12 @@
+package com.polycalc.mainpkg;
+
+public class CalculatorMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("The Java Library is on its way. Thanks for visiting");
+	}
+
+}
